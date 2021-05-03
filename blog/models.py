7b6bc20418762bed9bd5df7d87a36380193fc2cd
Jon Lee <39420293+4ka0@users.noreply.github.com>
2021-05-03 13:48:01 +0900

    Tags added

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 # Orderable adds sort_order to the images model to keep track of image order
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel, InlinePanel, MultiFieldPanel
+)
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
@@ -28,10 +32,26 @@ class BlogIndexPage(Page):
         return context
 
 
+class BlogPageTag(TaggedItemBase):
+    '''
+    Model for tags that can be attached to a blog entry. Has to be defined
+    prior to the BlogPage model.
+    '''
+
+    # ParentalKey is similar to ForeignKey in that it attaches this model to
+    # a parent model, but it also explicitly defines this model as being
+    # a child of BlogPage so that it is treated as a basic part of a page
+    # when submitting for moderation and tracking version history.
+    content_object = ParentalKey(
+        'BlogPage', related_name='tagged_items', on_delete=models.CASCADE
+    )
+
+
 class BlogPage(Page):
     date = models.DateField('Post date')
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -48,18 +68,17 @@ class BlogPage(Page):
     # Content panels are panels that can be edited by the user on the editing
     # interface for BlogPage.
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
+        MultiFieldPanel([  # Used to group multiple fields into single panel
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading='Blog information'),
         FieldPanel('intro'),
-        FieldPanel('body', classname='full'),
+        FieldPanel('body'),
         InlinePanel('gallery_images', label='Gallery images'),
     ]
 
 
 class BlogPageGalleryImage(Orderable):
-    # ParentalKey is similar to ForeignKey in that it attaches this model to
-    # a parent model, but it also explicitly defines this model as being
-    # a child of BlogPage so that it is treated as a basic part of a page
-    # when submitting for moderation and tracking version history.
     page = ParentalKey(
         BlogPage, on_delete=models.CASCADE, related_name='gallery_images'
     )
@@ -82,3 +101,15 @@ class BlogPageGalleryImage(Orderable):
         ImageChooserPanel('image'),
         FieldPanel('caption'),
     ]
+
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
